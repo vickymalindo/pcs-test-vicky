6e97123ac50da8837827507189fd9a5a4b895cb7
vickymalindo <vicky.malindo@gmail.com>
2023-10-23 22:17:04 +0700

    feat: add news data

---
+++ src/utils/news.ts
@@ -0,0 +1,26 @@
+export const newsData = [
+  {
+    name: 'Ana Riswati',
+    image: '<BsFillPersonFill />',
+    day: 'Senin',
+    date: '30 Mei 2022',
+    sentence:
+      '<h5> Kalimat 1 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, harum!</h5><br /><h5>Kalimat 2 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, harum!</h5><br /><h5>Kalimat 3 - Lorem ipsum dolor sit amet consectetur adipisicing elit Blanditiis, harum!</h5><br /><h5>Kalimat 4 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, harum!</h5>',
+  },
+  {
+    name: 'Annisa Zahra',
+    image: '<BsFillPersonFill />',
+    day: 'Senin',
+    date: '30 Mei 2022',
+    sentence:
+      '<h5> Kalimat 1 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, harum!</h5><br /><h5>Kalimat 2 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, harum!</h5><br /><h5>Kalimat 3 - Lorem ipsum dolor sit amet consectetur adipisicing elit Blanditiis, harum!</h5><br /><h5>Kalimat 4 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, harum!</h5>',
+  },
+  {
+    name: 'Nunik Kurmala',
+    image: '<BsFillPersonFill />',
+    day: 'Senin',
+    date: '30 Mei 2022',
+    sentence:
+      '<h5> Kalimat 1 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, harum!</h5><br /><h5>Kalimat 2 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, harum!</h5><br /><h5>Kalimat 3 - Lorem ipsum dolor sit amet consectetur adipisicing elit Blanditiis, harum!</h5><br /><h5>Kalimat 4 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, harum!</h5>',
+  },
+];
