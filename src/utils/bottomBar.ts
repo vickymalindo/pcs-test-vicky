f32a069e8777569fb97469bc69b969c6997ae1f7
vickymalindo <vicky.malindo@gmail.com>
2023-10-23 22:10:38 +0700

    feat: add bottom bar data

---
+++ src/utils/bottomBar.ts
@@ -0,0 +1,27 @@
+interface PropsBottomBarData {
+  text: string;
+  image: string;
+}
+
+export const bottomBarData: PropsBottomBarData[] = [
+  {
+    text: 'Home',
+    image: '<IoHome />',
+  },
+  {
+    text: 'Attendance',
+    image: '<BsCalendarCheck />',
+  },
+  {
+    text: 'Check Out',
+    image: '<FiLogOut />',
+  },
+  {
+    text: 'Form',
+    image: '<SiGoogleforms />',
+  },
+  {
+    text: 'Setting',
+    image: '<IoSettingsSharp />',
+  },
+];
