66b109921507757ddc28c56978d5b5ef571d65fb
vickymalindo <vicky.malindo@gmail.com>
2023-10-23 21:49:59 +0700

    feat: add notification data in utils

---
+++ src/utils/notification.ts
@@ -0,0 +1,70 @@
+// import {CgPill} from 'react-icons/cg'
+// import { FcAlarmClock } from 'react-icons/fc';
+// import { BsCheck, BsArrowRightShort } from 'react-icons/bs';
+// import { IoCloseOutline } from 'react-icons/io';
+
+export const dataNotification = [
+  {
+    image: '<FaCoins />',
+    title: 'Reimbursement',
+    date: 'today',
+    type: '<BsCheck />',
+    desc: 'Your submission "Lorem ipsum dolor ...." with the" with a total cost of 50,000 has been <b>paid</b>, please check your BRIMO application, thank you',
+  },
+  {
+    image: '<FaCoins />',
+    title: 'Reimbursement',
+    date: 'Yesterday',
+    type: '<IoCloseOutline />',
+    desc: 'Your submission description has been <b>rejected</b>, please click for details',
+  },
+  {
+    image: '<FaCoins />',
+    title: 'Reimbursement',
+    date: '2022-10-06',
+    type: '<BsArrowRightShort />',
+    desc: 'Your submission will be <b>processed</b> according to the reimbursement schedule. Please wait',
+  },
+  {
+    image: '<CgPill />',
+    title: 'Sickenss',
+    date: '2022-10-05',
+    type: '<BsCheck />',
+    desc: 'Your submission has been <b>approved</b> by the Superior',
+  },
+  {
+    image: '<CgPill />',
+    title: 'Sickenss',
+    date: '2022-10-05',
+    type: '<IoCloseOutline />',
+    desc: 'Your submission has been <b>rejected</b> please confirm with your Superior',
+  },
+  {
+    image: '<CgPill />',
+    title: 'Sickenss',
+    date: '2022-10-05',
+    type: '<BsArrowRightShort />',
+    desc: 'Your submission is being <b>reviewed</b> to the Superior for the Approvel process, please wait',
+  },
+  {
+    image: '<FcAlarmClock />',
+    title: 'Overtime',
+    date: '2022-10-05',
+    type: '<BsCheck />',
+    desc: 'Your submission has been <b>approved</b> by the Superior',
+  },
+  {
+    image: '<FcAlarmClock />',
+    title: 'Overtime',
+    date: '2022-10-05',
+    type: '<IoCloseOutline />',
+    desc: 'Your submission has been <b>rejected</b> please confirm with your Superior',
+  },
+  {
+    image: '<FcAlarmClock />',
+    title: 'Overtime',
+    date: '2022-10-05',
+    type: '<BsArrowRightShort />',
+    desc: 'Your submission is being <b>reviewed</b> to the Superior for the Approvel process, please wait',
+  },
+];
