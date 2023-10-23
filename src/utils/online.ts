5c3f99b57fb45c03c6908be504efa226f8870fb6
vickymalindo <vicky.malindo@gmail.com>
2023-10-23 22:26:39 +0700

    feat: add online data

---
+++ src/utils/online.ts
@@ -0,0 +1,48 @@
+interface PropsOnlineData {
+  src: string;
+  name: string;
+  location: string;
+}
+
+export const onlineData: PropsOnlineData[] = [
+  {
+    src: 'https://randomuser.me/api/portraits/men/1.jpg',
+    name: 'Jefrii',
+    location: 'Sahid',
+  },
+  {
+    src: 'https://randomuser.me/api/portraits/women/1.jpg',
+    name: 'Zasami',
+    location: 'BSD',
+  },
+  {
+    src: 'https://randomuser.me/api/portraits/men/11.jpg',
+    name: 'Sam',
+    location: 'Sahid',
+  },
+  {
+    src: 'https://randomuser.me/api/portraits/men/12.jpg',
+    name: 'Aldo',
+    location: 'Sahid',
+  },
+  {
+    src: 'https://randomuser.me/api/portraits/men/2.jpg',
+    name: 'Erwin',
+    location: 'Sahid',
+  },
+  {
+    src: 'https://randomuser.me/api/portraits/men/4.jpg',
+    name: 'Arye',
+    location: 'BSD',
+  },
+  {
+    src: 'https://randomuser.me/api/portraits/women/2.jpg',
+    name: 'Risya',
+    location: 'BSD',
+  },
+  {
+    src: 'https://randomuser.me/api/portraits/women/10.jpg',
+    name: 'Nesha',
+    location: 'WFH',
+  },
+];
