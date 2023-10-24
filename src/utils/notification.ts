1bba002cf4338e1294de726313cf80e16a216799
vickymalindo <vicky.malindo@gmail.com>
2023-10-24 13:13:02 +0700

    feat: add bgBlue property and tag p in desc

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -4,6 +4,7 @@ interface PropsNotification {
   date: string;
   type: string;
   desc: string;
+  bgBlue: boolean;
 }
 
 export const dataNotification: PropsNotification[] = [
@@ -12,62 +13,71 @@ export const dataNotification: PropsNotification[] = [
     title: 'Reimbursement',
     date: 'today',
     type: '<BsCheck />',
-    desc: 'Your submission "Lorem ipsum dolor ...." with the" with a total cost of 50,000 has been <b>paid</b>, please check your BRIMO application, thank you',
+    desc: '<p>Your submission "Lorem ipsum dolor ...." with the" with a total cost of 50,000 has been <b>paid</b>, please check your BRIMO application, thank you</p>',
+    bgBlue: true,
   },
   {
     image: '<FaCoins />',
     title: 'Reimbursement',
     date: 'Yesterday',
     type: '<IoCloseOutline />',
-    desc: 'Your submission description has been <b>rejected</b>, please click for details',
+    desc: '<p>Your submission description has been <b>rejected</b>, please click for details<p>',
+    bgBlue: false,
   },
   {
     image: '<FaCoins />',
     title: 'Reimbursement',
     date: '2022-10-06',
     type: '<BsArrowRightShort />',
-    desc: 'Your submission will be <b>processed</b> according to the reimbursement schedule. Please wait',
+    desc: '<p>Your submission will be <b>processed</b> according to the reimbursement schedule. Please wait</p>',
+    bgBlue: true,
   },
   {
     image: '<CgPill />',
     title: 'Sickenss',
     date: '2022-10-05',
     type: '<BsCheck />',
-    desc: 'Your submission has been <b>approved</b> by the Superior',
+    desc: '<p>Your submission has been <b>approved</b> by the Superior</p>',
+    bgBlue: true,
   },
   {
     image: '<CgPill />',
     title: 'Sickenss',
     date: '2022-10-05',
     type: '<IoCloseOutline />',
-    desc: 'Your submission has been <b>rejected</b> please confirm with your Superior',
+    desc: '<p>Your submission has been <b>rejected</b> please confirm with your Superior</p>',
+    bgBlue: false,
   },
   {
     image: '<CgPill />',
     title: 'Sickenss',
     date: '2022-10-05',
     type: '<BsArrowRightShort />',
-    desc: 'Your submission is being <b>reviewed</b> to the Superior for the Approvel process, please wait',
+    desc: '<p>Your submission is being <b>reviewed</b> to the Superior for the Approvel process, please wait</p>',
+    bgBlue: false,
   },
   {
     image: '<FcAlarmClock />',
     title: 'Overtime',
     date: '2022-10-05',
     type: '<BsCheck />',
-    desc: 'Your submission has been <b>approved</b> by the Superior',
+    desc: '<p>Your submission has been <b>approved</b> by the Superior</p>',
+    bgBlue: true,
   },
   {
     image: '<FcAlarmClock />',
     title: 'Overtime',
     date: '2022-10-05',
     type: '<IoCloseOutline />',
-    desc: 'Your submission has been <b>rejected</b> please confirm with your Superior',
+    desc: '<p>Your submission has been <b>rejected</b> please confirm with your Superior</p>',
+    bgBlue: false,
   },
   {
     image: '<FcAlarmClock />',
     title: 'Overtime',
     date: '2022-10-05',
     type: '<BsArrowRightShort />',
-    desc: 'Your submission is being <b>reviewed</b> to the Superior for the Approvel process, please wait',
+    desc: '<p>Your submission is being <b>reviewed</b> to the Superior for the Approvel process, please wait</p>',
+    bgBlue: false,
   },
 ];
